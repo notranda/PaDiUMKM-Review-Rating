c3cbc133019b07ff16026c6ba2dce46b02c12cab
Ogiana Ulandari <81963416+ogianaulandari@users.noreply.github.com>
2024-09-14 18:14:44 +0800

    Update AddReviews.jsx
    Update PopUp

--- client/src/pages/AddReviews.jsx
+++ client/src/pages/AddReviews.jsx
@@ -1,9 +1,11 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import hvs from '../assets/images/hvs.png';
 import pocky from '../assets/images/pocky.png';
 import kursi from '../assets/images/kursi.png';
 import SidebarRev from '../components/SidebarRev';
 import SidebarRev2 from '../components/SidebarRev2';
+import PopUpStar from '../components/PopUpStar';
 
 const orders = [
   {
@@ -16,7 +18,7 @@ const orders = [
     status: 'Dalam Pengiriman',
     date: '12 Mei 2023, 11:24 WIB',
     orderId: 'PO-2023-5-12-5238706',
-    image: pocky, 
+    image: pocky,
   },
   {
     id: 2,
@@ -43,7 +45,18 @@ const orders = [
 ];
 
 const AddReviews = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleAcceptOrder = () => {
+    setShowPopup(true);
+    // Navigasi ke halaman lain
+    // navigate('/form-review', { state: { productId: order.productId, userId: order.userId } });
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
 
   return (
     <div className="flex flex-row min-h-screen bg-gray-100">
@@ -82,8 +95,7 @@ const AddReviews = () => {
                   </button>
                   <button
                     className="text-white bg-teal-800 px-3 py-1 rounded"
-                    onClick={() => navigate('/form-review', { state: { productId: order.productId, userId: order.userId } })}
-                  >
+                    onClick={handleAcceptOrder}>
                     Terima Pesanan
                   </button>
                 </div>
@@ -96,10 +108,15 @@ const AddReviews = () => {
       <div className="w-1/4">
         <SidebarRev2 />
       </div>
+
+      {/* Popup */}
+      {showPopup && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <PopUpStar closePopup={closePopup} />
+        </div>
+      )}
     </div>
   );
 };
 
 export default AddReviews;
-
-
